1f2afa207ce064d323fe674e1cd9539553db92e2
Amar <amarpreetsingh1822@gmail.com>
2020-11-03 14:43:52 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import img1 from './img1.png';
+import img2 from './img2.png';
+import img3 from './img3.png';
+import img4 from './img4.png';
+import img5 from  './img5.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+       <header>
+         <img src={img1} className='1-logo' alt='1st-image' />
+       </header>
+       <div>
+       <img src={img2} className='1-logo' alt='1st-image' />
+       </div>
+       <div>
+       <img src={img3} className='1-logo' alt='1st-image' />
+       </div>
+       <div>
+       <img src={img4} className='1-logo' alt='1st-image' />
+       </div>
+       <div>
+       <img src={img5} className='1-logo' alt='1st-image' />
+       </div>
+       <p>&copy; 2020. All Rights Reserved Biryanis & More .Privacy Policy .Terms & Conditions </p>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
